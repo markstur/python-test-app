67733a3a2357f73ffb749947fd378d8d01a4164a
Mark Sturdevant <mark.sturdevant@ibm.com>
2021-01-22 15:08:37 -0800

    More chown uid/gid testing

--- examples/mod-wsgi-test-app/wsgi.py
+++ examples/mod-wsgi-test-app/wsgi.py
@@ -1,27 +1,39 @@
 import os
+import pwd
 from flask import Flask
 application = Flask(__name__)
 
 def test_chown():
-    testfile = "/tmp/testfile.txt"
     print("TEST1")
+    testfile = "/tmp/testfile.txt"
 
-    os.close(os.open(testfile, os.O_CREAT))
     print("TEST2")
+    os.close(os.open(testfile, os.O_CREAT))
 
+    print("TEST3")
     print("st_uid:", os.stat(testfile).st_uid)
     print("st_gid:", os.stat(testfile).st_gid)
-    print("TEST3")
 
-    uid = 501
-    gid = 20
+    print("TEST4")
+    # uid = os.getuid()
+    # gid = os.getgid()
+    nobody = pwd.getpwnam("nobody")
+    uid = nobody.pw_uid  # os.getuid()
+    gid = nobody.pw_gid  # os.getgid()
     os.chown(testfile, uid, gid)
     print("os.chown", testfile, uid, gid)
-    print("TEST4")
 
+    print("TEST5")
     print("st_uid:", os.stat(testfile).st_uid)
     print("st_gid:", os.stat(testfile).st_gid)
-    print("TEST5")
+
+    print("TEST6")
+    uid = os.getuid()
+    gid = os.getgid()
+    os.chown(testfile, uid, gid)
+    print("os.chown", testfile, uid, gid)
+    print("st_uid:", os.stat(testfile).st_uid)
+    print("st_gid:", os.stat(testfile).st_gid)
 
 @application.route('/')
 def hello():
