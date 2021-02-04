28617d23d49f2e8411ea843d0dc36d7b360d7ba7
Mark Sturdevant <mark.sturdevant@ibm.com>
2021-02-04 15:29:07 -0800

    Test page with exception catching and some formatting
    * Try/catch so that the page shows up even when errors happen
    * Some formatting for the message that are shown on the page and in the log.

--- README.md
+++ README.md
@@ -1,53 +1,10 @@
-# python-test-app
+# scc-tutorial-assets
 
-## Commands to run
+This repo contains an example app and other assets referred to by the Security Context Contraints (SCC) Tutorial.
 
-```bash
-oc login...
-oc new-app https://github.com/markstur/python-test-app.git 
-oc start-build python-test-app  
-oc expose svc/python-test-app
+* Link to tutorial TBD
 
-```
+## Demo applications
 
-## Example with output
-
-```bash
-% oc new-app https://github.com/markstur/python-test-app.git
---> Found image ae420ac (44 hours old) in image stream "markstur-project1/python" under tag "latest" for "python"
-
-    Python 3.6 
-    ---------- 
-    Python 3.6 available as container is a base platform for building and running various Python 3.6 applications and frameworks. Python is an easy to learn, powerful programming language. It has efficient high-level data structures and a simple but effective approach to object-oriented programming. Python's elegant syntax and dynamic typing, together with its interpreted nature, make it an ideal language for scripting and rapid application development in many areas on most platforms.
-
-    Tags: builder, python, python36, python-36, rh-python36
-
-    * The source repository appears to match: python
-    * A source build using source code from https://github.com/markstur/python-test-app.git will be created
-      * The resulting image will be pushed to image stream tag "python-test-app:latest"
-      * Use 'start-build' to trigger a new build
-    * This image will be deployed in deployment config "python-test-app"
-    * Port 8080/tcp will be load balanced by service "python-test-app"
-      * Other containers can access this service through the hostname "python-test-app"
-
---> Creating resources ...
-    imagestream.image.openshift.io "python-test-app" created
-    buildconfig.build.openshift.io "python-test-app" created
-    deploymentconfig.apps.openshift.io "python-test-app" created
-    service "python-test-app" created
---> Success
-    Build scheduled, use 'oc logs -f bc/python-test-app' to track its progress.
-    Application is not exposed. You can expose services to the outside world by executing one or more of the commands below:
-     'oc expose svc/python-test-app' 
-    Run 'oc status' to view your app.
-
-```
-
-```bash
-$ oc start-build python-test-app  
-build.build.openshift.io/python-test-app-2 started
-```
-
-
-
+[Python using WSGI and Flask](examples/mod-wsgi-test-app/README.md)
 
--- examples/mod-wsgi-test-app/wsgi.py
+++ examples/mod-wsgi-test-app/wsgi.py
@@ -1,40 +1,84 @@
 import os
-import pwd
+import logging
+
 from flask import Flask
+
+TEST_FILE = "/var/opt/lib/scc-tutorial/data/testfile.txt"
+INFO = logging.INFO
+ERROR = logging.ERROR
+DEBUG = logging.DEBUG
+BULLETS = {
+    INFO: ' * ',
+    ERROR: '!!! ',
+    DEBUG: ' '
+}
+TAGS = {
+    INFO: 'code',
+    ERROR: 'b',
+    DEBUG: 'small'
+}
+
+logging.basicConfig(level=logging.INFO)
+
 application = Flask(__name__)
 
-def test_chown():
-    print("TEST1")
-    testfile = os.path.expanduser("~/testfile.txt")
 
-    print("TEST2")
-    os.close(os.open(testfile, os.O_CREAT))
-    
-    print("TEST3")
-    print("st_uid:", os.stat(testfile).st_uid)
-    print("st_gid:", os.stat(testfile).st_gid)
+def tests():
 
-    print("TEST4")
+    msgs = ["<h1>Testing for expected security context</h1>"]
+
+    def msg(level, message):
+
+        b = BULLETS[level]
+        t = TAGS[level]
+        application.logger.log(level, f' {b} {message} {b}')
+        msgs.append(f"<{t}>{message}</{t}><br/>")
+
+    # Test: Who am I?
+    msgs.append("<h3>User ID Testing</h3>")
+    expected_uid = 1234
+    msg(INFO, f"This app expects to run with user ID = {expected_uid}")
     uid = os.getuid()
     gid = os.getgid()
+    msg(INFO, f"Actual user ID = {uid}")
+    msg(DEBUG, f"Actual group ID = {gid}")
+    try:
+        assert uid == expected_uid, f"UID {uid} != {expected_uid}"
+        msg(INFO, "SUCCESS: Running as the expected user ID")
+    except Exception as e:
+        msg(ERROR, repr(e))
 
+    msgs.append("<h3>File Write Testing</h3>")
+    msg(INFO, f"This app expects to write a file in {TEST_FILE}")
+    try:
+        os.close(os.open(TEST_FILE, os.O_CREAT))
+        msg(INFO, f"Created file: {TEST_FILE}")
+        msg(INFO, f"Test file name: {TEST_FILE}")
+        msg(INFO, f"Test file owner: {os.stat(TEST_FILE).st_uid}")
+        msg(INFO, f"Test file group: {os.stat(TEST_FILE).st_gid}")
+        msg(INFO, f"os.chown {TEST_FILE} -1:5678")
+        os.chown(TEST_FILE, -1, 5678)
+        msg(INFO, f"st_uid: {os.stat(TEST_FILE).st_uid}")
+        msg(INFO, f"st_gid: {os.stat(TEST_FILE).st_gid}")
+        os.chown(TEST_FILE, uid, gid)
+        msg(INFO, f"os.chown {TEST_FILE}, {uid}, {gid}")
+        msg(INFO, f"st_uid: {os.stat(TEST_FILE).st_uid}")
+        msg(INFO, f"st_gid: {os.stat(TEST_FILE).st_gid}")
+    except Exception as e:
+        msg(ERROR, repr(e))
 
-    print("TEST5")
-    print("os.chown", testfile, -1, 5678)
-    os.chown(testfile, -1, 5678)
-    print("st_uid:", os.stat(testfile).st_uid)
-    print("st_gid:", os.stat(testfile).st_gid)
+    # For pod log print all the messages one at a time
+    # for m in msgs:
+        # application.logger.log(INFO, m)
 
-    print("TEST6")
-    os.chown(testfile, uid, gid)
-    print("os.chown", testfile, uid, gid)
-    print("st_uid:", os.stat(testfile).st_uid)
-    print("st_gid:", os.stat(testfile).st_gid)
+    # Return the messages "formatted" for the web page
+    return ''.join(msgs)
+
 
 @application.route('/')
 def hello():
-    test_chown()
-    return b'Hello World from mod_wsgi hosted WSGI application!'
+    return tests()
+
 
 if __name__ == '__main__':
     application.run()
